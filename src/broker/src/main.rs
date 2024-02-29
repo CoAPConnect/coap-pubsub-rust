c732f57967f7ce77012f9dfa0b80aba3363b61b7
Karri <karri.seppa@tuni.fi>
2024-02-29 20:51:55 +0200

    remove commented/unuseful code from main

--- src/broker/src/main.rs
+++ src/broker/src/main.rs
@@ -7,11 +7,9 @@ use resource::Topic;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
-
 struct Subscriber {
     addr: SocketAddr,
 }
-
 
 type TopicMap = Arc<Mutex<HashMap<String, Topic>>>;
 
@@ -32,33 +30,6 @@ fn handle_delete(req:&Box<CoapRequest<SocketAddr>>){
 }
 
 fn main() {
-    //Topic testing
-    let mut example_topic = Topic::new(String::from("topic1"), String::from("core.ps.conf"));
-    example_topic.set_topic_uri(String::from("http://example.com/topic1"));
-    example_topic.set_topic_data(String::from("data"));
-    example_topic.set_media_type(String::from("text/plain"));
-    example_topic.set_topic_type(String::from("topic"));
-    example_topic.set_expiration_date(String::from("2022-12-31"));
-    example_topic.set_max_subscribers(100);
-    println!("Topic name: {}", example_topic.get_topic_name());
-    println!("Resource type: {}", example_topic.get_resource_type());
-    println!("Topic URI: {}", example_topic.get_topic_uri());
-    println!("Topic data: {}", example_topic.get_topic_data());
-    println!("Media type: {}", example_topic.get_media_type());
-    println!("Topic type: {}", example_topic.get_topic_type());
-    println!("Expiration date: {}", example_topic.get_expiration_date());
-    println!("Max subscribers: {}", example_topic.get_max_subscribers());
-    let mut topic2 = Topic::new(String::from("topic2"), String::from("core.ps.conf"));
-    println!("Topic name: {}", topic2.get_topic_name());
-    println!("Resource type: {}", topic2.get_resource_type());
-    println!("Topic URI: {}", topic2.get_topic_uri());
-    println!("Topic data: {}", topic2.get_topic_data());
-    println!("Media type: {}", topic2.get_media_type());
-    println!("Topic type: {}", topic2.get_topic_type());
-    println!("Expiration date: {}", topic2.get_expiration_date());
-    println!("Max subscribers: {}", topic2.get_max_subscribers());
-    
-    //Topic testing ends
     let topics: TopicMap = Arc::new(Mutex::new(HashMap::new()));
     let addr = "127.0.0.1:5683";
     Runtime::new().unwrap().block_on(async move {
@@ -88,30 +59,3 @@ fn main() {
         }).await.unwrap();
     });
 }
-
-
-//let _ = inform_client("127.0.0.1:5684", request.message.payload.clone()).await;
-/*
-async fn inform_client(client_addr: &str, payload: Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
-    let client_addr: SocketAddr = client_addr.parse()?;
-    
-    let socket = UdpSocket::bind("0.0.0.0:0")?;
-    socket.connect(client_addr)?;
-
-    let mut packet = Packet::new();
-    packet.header.set_type(coap_lite::MessageType::Confirmable);
-    packet.header.code = MessageClass::Request(Method::Post); 
-    packet.payload = payload.clone();
-
-    let message = packet.to_bytes()?;
-    socket.send(&message)?;
-
-    let mut buffer = [0; 1024];
-    let (size, _) = socket.recv_from(&mut buffer)?;
-    let response_packet = Packet::from_bytes(&buffer[..size])?;
-    println!("Received response: {:?}", response_packet);
-
-    Ok(())
-
-}
-*/
